544ec3b9dfee4151fc3756e97bdef7e28d688aed
mrnoob101101 <mazhor101@ya.ru>
2022-01-26 20:11:50 +0200

    Added localstorage by redux-persist

--- src/index.js
+++ src/index.js
@@ -6,17 +6,23 @@ import { store } from './store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Game } from './components/Game/Game';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/game">
-            <Game />
-          </Route>
-        </Switch>
+        <PersistGate loading={null} persistor={persistor}>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/game">
+              <Game />
+            </Route>
+          </Switch>
+        </PersistGate>
       </Provider>
     </React.StrictMode>
   </BrowserRouter>,
--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,27 @@ import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './root.reducer';
 import createSagaMiddleware from 'redux-saga';
 import { gamesSaga } from './games.sagas';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 
 const saga = createSagaMiddleware();
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedreducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: [saga],
+  reducer: persistedreducer,
+  middleware: [saga]
 });
 saga.run(gamesSaga);
+
+/*
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [saga]
+});
+saga.run(gamesSaga);*/
